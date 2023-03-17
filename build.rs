f185b4b598423b66af71d710b1c013c56dac3452
Hengqi Chen <hengqi.chen@gmail.com>
2023-03-17 13:49:42 +0000

    Initial commit
    Signed-off-by: Hengqi Chen <hengqi.chen@gmail.com>

---
+++ build.rs
@@ -0,0 +1,30 @@
+use std::env;
+use std::path;
+use std::process;
+
+fn main() {
+    let src_dir = path::PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
+    let xdptools_dir = src_dir.join("xdp-tools");
+    let libxdp_dir = xdptools_dir.join("lib/libxdp");
+    let headers_dir = xdptools_dir.join("headers/xdp");
+
+    let status = process::Command::new("make")
+        .arg("libxdp")
+        .current_dir(&xdptools_dir)
+        .status()
+        .expect("could not execute make");
+
+    assert!(status.success(), "make failed");
+
+    println!("cargo:include={}", headers_dir.display());
+    println!("cargo:rustc-link-search={}", libxdp_dir.display());
+    println!("cargo:rustc-link-lib=static=xdp");
+
+    bindgen::Builder::default()
+        .header("bindings.h")
+        .clang_arg(format!("-I{}", headers_dir.display()))
+        .generate()
+        .expect("Unable to generate bindings")
+        .write_to_file(src_dir.join("src/bindings.rs"))
+        .expect("Couldn't write bindings");
+}
---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(clippy::missing_safety_doc)]
+#![allow(clippy::useless_transmute)]
+
+include!("bindings.rs");
