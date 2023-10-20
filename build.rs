0969af5a9148d200c033a21561b4da25a9a9bb76
d0nut <nathanial.lattimer@gmail.com>
2023-10-19 20:00:19 -0700

    Fix linking static inline functions and update xdp-tools to v1.4.0
    Signed-off-by: Nathanial Lattimer <d0nut@resync.gg>

--- build.rs
+++ build.rs
@@ -8,6 +8,9 @@ fn main() {
     let libxdp_dir = xdptools_dir.join("lib/libxdp");
     let headers_dir = xdptools_dir.join("headers/xdp");
 
+    let libbpf_dir = xdptools_dir.join("lib/libbpf/src");
+    let bpf_headers_dir = libbpf_dir.join("root/usr/include");
+
     let status = process::Command::new("make")
         .arg("libxdp")
         .current_dir(&xdptools_dir)
@@ -16,12 +19,17 @@ fn main() {
 
     assert!(status.success(), "make failed");
 
-    println!("cargo:include={}", headers_dir.display());
+    println!("cargo:rustc-link-search={}", libbpf_dir.display());
     println!("cargo:rustc-link-search={}", libxdp_dir.display());
+    println!("cargo:rustc-link-lib=static=bpf");
     println!("cargo:rustc-link-lib=static=xdp");
+    println!("cargo:rustc-link-lib=elf");
+    println!("cargo:rustc-link-lib=z");
 
     bindgen::Builder::default()
         .header("bindings.h")
+        .generate_inline_functions(true)
+        .clang_arg(format!("-I{}", bpf_headers_dir.display()))
         .clang_arg(format!("-I{}", headers_dir.display()))
         .generate()
         .expect("Unable to generate bindings")
