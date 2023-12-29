b482e6c892bc5cef08a51b5354f8e0b4867da339
Hengqi Chen <ritchiechen@tencent.com>
2023-12-29 07:46:33 +0000

    libxdp-sys v0.3.0 release
    The previous commit seems broken, this patch:
      * Drop libbpf dependencies
      * Bump bindgen to 0.69.1
      * Bump xdp-tools to v1.4.1
    
    Signed-off-by: Hengqi Chen <ritchiechen@tencent.com>

--- build.rs
+++ build.rs
@@ -6,10 +6,8 @@ fn main() {
     let src_dir = path::PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
     let xdptools_dir = src_dir.join("xdp-tools");
     let libxdp_dir = xdptools_dir.join("lib/libxdp");
-    let headers_dir = xdptools_dir.join("headers/xdp");
-
     let libbpf_dir = xdptools_dir.join("lib/libbpf/src");
-    let bpf_headers_dir = libbpf_dir.join("root/usr/include");
+    let headers_dir = xdptools_dir.join("headers/xdp");
 
     let status = process::Command::new("make")
         .arg("libxdp")
@@ -21,7 +19,6 @@ fn main() {
 
     println!("cargo:rustc-link-search={}", libbpf_dir.display());
     println!("cargo:rustc-link-search={}", libxdp_dir.display());
-    println!("cargo:rustc-link-lib=static=bpf");
     println!("cargo:rustc-link-lib=static=xdp");
     println!("cargo:rustc-link-lib=elf");
     println!("cargo:rustc-link-lib=z");
@@ -29,7 +26,6 @@ fn main() {
     bindgen::Builder::default()
         .header("bindings.h")
         .generate_inline_functions(true)
-        .clang_arg(format!("-I{}", bpf_headers_dir.display()))
         .clang_arg(format!("-I{}", headers_dir.display()))
         .generate()
         .expect("Unable to generate bindings")
